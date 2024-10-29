5c474fdfe5b7a980c404b2814414cf1a7ecd1f91
smart <olubunmiolorunda@gmail.com>
2024-10-30 00:13:35 +0100

    Initial commit

--- Flash_Card/src/App.jsx
+++ Flash_Card/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react';
 import "./App.css";
 
 export default function App() {
@@ -10,67 +9,79 @@ export default function App() {
   );
 }
 
-
-let questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript"
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components"
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props"
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook"
-  },
-  {
-    id: 2002,
-    question:
-    "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
-];
-
 function FlashCards() {
+  const [questions, setQuestions] = useState([
+
+    {
+      id: 3457,
+      question: "What language is React based on?",
+      answer: "JavaScript"
+    },
+    {
+      id: 7336,
+      question: "What are the building blocks of React apps?",
+      answer: "Components"
+    },
+    {
+      id: 8832,
+      question: "What's the name of the syntax we use to describe a UI in React?",
+      answer: "JSX"
+    },
+    {
+      id: 1297,
+      question: "How to pass data from parent to child components?",
+      answer: "Props"
+    },
+    {
+      id: 9103,
+      question: "How to give components memory?",
+      answer: "useState hook"
+    },
+    {
+      id: 2002,
+      question:
+      "What do we call an input element that is completely synchronised with state?",
+      answer: "Controlled element"
+    }
+  ]);
+
   const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+
+  function deleteBlog(id) {
+    // Filter out the question with the specified ID
+    const updatedQuestions = questions.filter(question => question.id !== id);
+    setQuestions(updatedQuestions);
+  }
+
+  const [name, setName] = useState('mario')
+useEffect(() => {
+  alert('welcome back')
   
-  function handleClick(id){
-    setSelectedId(id != selectedId ? id : null);
-    
-  }
+},[name])//empty depencies makes us render the useffect function once 
 
-  const DeleteBlog = (id) => {
- const blog = questions.filter(newBlog => newBlog.id !== id);
-setMe(blog);
-
-  }
-
-return( <div className='flashcards'>
-    {questions.map(blog => 
-    <div key={blog.id} className={blog.id === selectedId ? "selected" : ""} 
-    onClick={() => handleClick(blog.id)}> <p>
-      {blog.id === selectedId ? blog.answer : blog.question}
-      </p>
-      <button onClick={()=> {
-        DeleteBlog(blog.id) 
-      }}>Delete Blog</button>
-      </div>
-    
-    )}
-  </div>
-)
+  return (
+    <div className='flashcards'>
+      {questions.map(blog => 
+        <div key={blog.id} className={blog.id === selectedId ? "selected" : ""} 
+          onClick={() => handleClick(blog.id)}>
+          <p>
+            {blog.id === selectedId ? blog.answer : blog.question}
+          </p>
+          <button onClick={() => {
+           
+            deleteBlog(blog.id);
+          }}>
+            Delete Blog
+          </button>
+       <button onClick={() => {setName('Amanuel')}}>Click to change name</button>
+       {name}
+        </div>
+      )}
+    </div>
+  );
 }
+
